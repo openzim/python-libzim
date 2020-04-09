00f6a6dc288346d47b838e0b5f289a14239ebd87
Juan Diego Caballero <jdc@monadical.com>
2020-04-09 11:09:39 -0500

    Remove get_article_properties()

--- pyzim/pyzim.pyx
+++ pyzim/pyzim.pyx
@@ -102,10 +102,6 @@ cdef class ZimArticle:
         # Set new internal C zim.ZimArticle article
         self.c_zim_article = art
 
-
-    def get_article_properties(self):
-        return dict((name, getattr(self, name)) for name in dir(self) if not name.startswith('__') )
-
     # Factory functions - Currently Cython can't use classmethods
     @staticmethod
     cdef from_read_article(zim.Article art):
