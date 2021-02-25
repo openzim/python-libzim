fe85a733ace5909a1d5e39ec69a99f7c6ee17556
rgaudin <rgaudin@gmail.com>
2021-02-25 11:13:02 +0000

    Added Python 3.9 support (#101)
    Using newest, python3.9-compatible Cython 0.29.22

--- setup.py
+++ setup.py
@@ -152,6 +152,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         # "Typing :: Typed",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
