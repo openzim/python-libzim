4ef434402ed3d7b0fcd497f22e318b58c4430905
Matthieu Gautier <mgautier@kymeria.fr>
2021-09-14 16:15:55 +0200

    Add move constructor from base to the Wrapper.
    This is needed as `zim::Search` cannot be copied.

--- libzim/libwrapper.h
+++ libzim/libwrapper.h
@@ -78,6 +78,9 @@ class Wrapper {
     Wrapper(const Base& base)
       : mp_base(new Base(base))
     {}
+    Wrapper(Base&& base)
+      : mp_base(new Base(std::move(base)))
+    {}
     Wrapper(Wrapper&& other) = default;
     Wrapper& operator=(Wrapper&& other) = default;
   protected:
