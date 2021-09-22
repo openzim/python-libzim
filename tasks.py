7b76ca08f72b2f9fbb522da8d9deffcf1eaae676
Matthieu Gautier <mgautier@kymeria.fr>
2021-09-22 16:25:17 +0200

    Replace rebuild.sh by a task.py
    `task.py` is a description file for invoke (https://www.pyinvoke.org/)

---
+++ tasks.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+
+"""
+A description file for invoke (https://www.pyinvoke.org/)
+"""
+
+from invoke import task
+
+
+@task
+def build_ext(c):
+    c.run("python setup.py build_ext -i")
+
+
+@task
+def test(c):
+    c.run(f"python -m pytest --color=yes --ff")
+
+
+@task
+def clean(c):
+    c.run("rm -rf build")
+    c.run("rm *.so")
+
+
+@task
+def install_dev(c):
+    c.run("pip install -r requirements-dev.txt")
+
+
+@task
+def lint(c):
+    c.run("isort .")
+    c.run("black .")
+    c.run("flake8 .")
+
+
+if __name__ == "__main__":
+    print("""\
+This file is not intended to be directly run.
+Install invoke and run the `invoke` command line.
+""")
