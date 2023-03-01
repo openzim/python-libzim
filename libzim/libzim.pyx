06340864df3bd2f8631be90c4ad2d1363675d840
renaud gaudin <reg@rskg.org>
2023-03-01 11:58:05 +0000

    Improved python API
    - Better docstring
    - Refactored to uses module functions instead of (useless) Version class
    - Changed get_versions() to return an OrderedDict for easier in-python use
    - Added get_libzim_version() shortcut
    - Simplified print_version's code
    - Fixed print_version tests (use flush so we're sure its present when checking)
    - Changed tests for get_versions and get_libzim_version

--- libzim/libzim.pyx
+++ libzim/libzim.pyx
@@ -39,6 +39,7 @@ import os
 import pathlib
 import sys
 import traceback
+from collections import OrderedDict
 from types import ModuleType
 from typing import Dict, Generator, Iterator, List, Optional, Set, Tuple, Union
 from uuid import UUID
@@ -1244,37 +1245,46 @@ suggestion_public_objects = [
 suggestion = create_module(suggestion_module_name, suggestion_module_doc, suggestion_public_objects)
 
 version_module_doc = """libzim version module
-- Get versions of libzim.
-- Print version of libzim.
+- Get version of libzim and its dependencies
+- Print version of libzim and its dependencies
+- Get libzim version
 
 Usage:
-    versions = Version.get_versions()
-    Version.print_version()"""
+    from libzim.version import get_libzim_version, get_versions, print_versions
+    major, minor, patch = get_libzim_version().split(".", 2)
+
+    for dependency, version in get_versions().items():
+        print(f"- {dependency}={version}")
+
+    print_versions()"""
+
+
+def print_versions(out: Union[sys.stdout, sys.stderr] = sys.stdout):
+    """print libzim and its dependencies list with their versions"""
+    for library, version in get_versions().items():
+        prefix = "" if library == "libzim" else "+ "
+        print(f"{prefix}{library} {version}", file=out or sys.stdout)
+
+
+def get_versions() -> OrderedDict[str, str]:
+    """ library: version mapping. Always includes `libzim`"""
+    versions = zim.getVersions()
+    return OrderedDict({
+        library.decode("UTF-8"): version.decode("UTF-8")
+        for library, version in versions
+    })
+
+def get_libzim_version() -> str:
+    """libzim version string"""
+    return get_versions()["libzim"]
+
 version_public_objects = [
-    Version,
+    get_libzim_version,
+    get_versions,
+    print_versions,
 ]
 version_module_name = f"{__name__}.version"
 version = create_module(version_module_name, version_module_doc, version_public_objects)
-
-cdef class Version:
-    __module__ = version_module_name
-    @staticmethod
-    def get_versions() -> [(bytes, bytes)]:
-        return zim.getVersions()
-
-    @staticmethod
-    def print_version(out: Union[sys.stdout, sys.stderr] = sys.stdout):
-        versions = Version.get_versions()
-        for i, version in enumerate(versions):
-            prefix = ""
-            if i > 0:
-                prefix=  "+"
-            output = "{prefix} {version_name} {version_value}".format(
-                    prefix=prefix, 
-                    version_name=version[0].decode("utf-8"), 
-                    version_value=version[1].decode("utf-8"))
-            print(output, file=out)
-
 
 
 class ModuleLoader(importlib.abc.Loader):
--- tests/test_libzim_version.py
+++ tests/test_libzim_version.py
@@ -1,20 +1,33 @@
+import re
 import sys
 
-from libzim.version import Version
+from libzim.version import get_libzim_version, get_versions, print_versions
 
 
-def test_get_verions():
-    versions = Version.get_versions()
-    assert len(versions) != 0
-
-
-def test_print_verion_with_stdout(capfd):
-    Version.print_version()
-    stdout, stderr = capfd.readouterr()
+def test_version_print_version_with_stdout(capsys):
+    print_versions()
+    print("", file=sys.stdout, flush=True)
+    stdout, stderr = capsys.readouterr()
     assert len(stdout) != 0
 
 
-def test_print_verion_with_stderr(capfd):
-    Version.print_version(sys.stderr)
-    stdout, stderr = capfd.readouterr()
+def test_version_print_version_with_stderr(capsys):
+    print_versions(sys.stderr)
+    print("", file=sys.stderr, flush=True)
+    stdout, stderr = capsys.readouterr()
     assert len(stderr) != 0
+
+
+def test_get_versions():
+    versions = get_versions()
+    assert versions
+    assert "libzim" in versions
+    assert len(versions.keys()) > 1
+    for library, version in versions.items():
+        assert isinstance(library, str)
+        assert isinstance(version, str)
+
+
+def test_get_libzim_version():
+    # libzim uses semantic versioning
+    assert re.match(r"\d+\.\d+\.\d+", get_libzim_version())
