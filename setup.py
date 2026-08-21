7d7c7aa77795ae0f9c9c7e6ebcd7771aad314a1c
benoit74 <benoit74@users.noreply.github.com>
2026-08-21 18:50:00 +0000

    Fix .so extension

--- setup.py
+++ setup.py
@@ -468,6 +468,15 @@ def get_cython_extension() -> list[Extension]:
 
 
 class LibzimBuildExt(build_ext):
+
+    def get_ext_filename(self, ext_name):
+        # set proper abi3 extension
+        filename = super().get_ext_filename(ext_name)
+        if config.can_use_limited_api:
+            ext_suffix = sysconfig.get_config_var("EXT_SUFFIX")
+            filename = filename[: -len(ext_suffix)] + ".abi3" + os.path.splitext(ext_suffix)[1]
+        return filename
+    
     def finalize_options(self):
         """Workaround for rpath bug in distutils for macOS"""
         super().finalize_options()
